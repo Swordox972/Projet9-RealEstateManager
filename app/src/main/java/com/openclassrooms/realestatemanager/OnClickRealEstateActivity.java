37d057a046c1a675d78caf2457d2df9ed5f2f5cc
Loïc Jackotin <Bleucielsky@outlook.fr>
2020-11-09 17:58:42 -0400

    Lors d'un clique sur un bien immobilier, une image affichera le bien en plus détaillé. De plus, cela affichera aussi le prix, la date d'entrée du bien sur le marché, le status du bien (à vendre ou vendu) et le date de vente si il a été vendu

--- app/src/main/java/com/openclassrooms/realestatemanager/OnClickRealEstateActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/OnClickRealEstateActivity.java
@@ -1,11 +1,12 @@
 package com.openclassrooms.realestatemanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 import com.openclassrooms.realestatemanager.databinding.ActivityOnClickRealEstateBinding;
 import com.openclassrooms.realestatemanager.model.RealEstate;
 
@@ -21,28 +22,55 @@ public class OnClickRealEstateActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        initializeRealEstateValueAndDescription();
+        initializeRealEstateValueAndDescriptionAndImageView();
         initializeRealEstateIconsValue();
 
     }
 
-    private void initializeRealEstateValueAndDescription() {
-        Intent intent= getIntent();
+    private void initializeRealEstateValueAndDescriptionAndImageView() {
+        Intent intent = getIntent();
         mRealEstate = (RealEstate) intent.getSerializableExtra(RealEstateFragment.KEY);
+
+        Glide.with(binding.activityOnClickRealEstateImageView.getContext())
+                .load(mRealEstate.getPhotoUrl())
+                .into(binding.activityOnClickRealEstateImageView);
 
         binding.activityOnClickRealEstateDescription.setText(mRealEstate.getDescription());
     }
 
     private void initializeRealEstateIconsValue() {
+        String status;
+        if (mRealEstate.getStatus() == RealEstate.Status.forSell) {
+            status = "For sell";
+            binding.activityOnClickRealEstateStatus.setTextColor(getResources()
+                    .getColor(R.color.activity_on_click_real_estate_for_sell_status_color));
+        } else {
+            status = "Sold";
+            binding.activityOnClickRealEstateStatus.setTextColor(getResources()
+                    .getColor(R.color.activity_on_click_real_estate_sold_status_color));
+        }
+
+        String dateOfSale;
+        if (mRealEstate.getDateOfSale() == null) {
+            dateOfSale = "None";
+        } else {
+            dateOfSale = mRealEstate.getDateOfSale();
+        }
+
         String surface = mRealEstate.getSurface() + " sq" + " m";
-        String numberOfRooms =Integer.toString(mRealEstate.getNumberOfRooms());
+        String numberOfRooms = Integer.toString(mRealEstate.getNumberOfRooms());
         String numberOfBathrooms = Integer.toString(mRealEstate.getNumberOfBathrooms());
         String numberOfBedrooms = Integer.toString(mRealEstate.getNumberOfBedrooms());
 
+
+        binding.activityOnClickRealEstateStatus.setText(status);
         binding.activityOnClickRealEstateSurfaceValue.setText(surface);
         binding.activityOnClickRealEstateRoomsValue.setText(numberOfRooms);
         binding.activityOnClickRealEstateBathroomsValue.setText(numberOfBathrooms);
         binding.activityOnClickRealEstateBedroomsValue.setText(numberOfBedrooms);
         binding.activityOnClickRealEstateLocationValue.setText(mRealEstate.getSecondLocation());
+        binding.activityOnClickRealEstatePriceValue.setText(mRealEstate.getPrice());
+        binding.activityOnClickRealEstateEntryDateValue.setText(mRealEstate.getEntryDate());
+        binding.activityOnClickRealEstateSaleDateValue.setText(dateOfSale);
     }
 }
